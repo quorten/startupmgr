7f38bae7c63f6ff50037bda2e697715b97c77e49
Andrew Makousky <mako0042@umn.edu>
2011-07-12 19:44:00 -0500

    Save file.

---
+++ resource.h
@@ -0,0 +1,19 @@
+//Contains resource definitions for the user interface
+
+#define MAIN_ICON		101
+#define STARTUP_INFO	102
+#define WIZ_01			103
+#define WIZ_02			104
+#define D_STATIC1		2001
+#define D_STATIC2		2002
+#define D_LOADNOTHING	2003
+#define D_OFFLLOAD		2004
+#define D_NETSTART		2005
+#define D_LOADALL		2006
+#define D_CUSTOMIZE		2007
+#define D_STATIC3		2008
+#define D_STATIC4		2009
+#define D_PROCBOX		2010
+#define D_STATIC5		2011
+#define D_SVCBOX		2012
+#define D_STATIC6		2013
